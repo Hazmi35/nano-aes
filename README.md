0299875c0f7a62c2987809d64ee29d9aa86a4d11
Hazmi Alfarizqi <contact@hzmi.xyz>
2023-04-01 01:24:55 +0700

    chore: initialize repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# nano-aes
+
+A simple AES (Advanced Encryption Standard) module. Wrapper for Node.js crypto module
---
+++ tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    target: "es2022",
+    minify: "terser",
+    format: ["esm", "cjs"],
+    dts: true
+});
